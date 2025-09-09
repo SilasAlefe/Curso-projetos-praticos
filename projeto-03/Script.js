9788fecad9dc86c5ca380a771d3d9462803a123c
Silas Álefe <116173586+SilasAlefe@users.noreply.github.com>
2025-09-09 18:19:45 -0300

    09/09/2025-18:19

--- projeto-03/Script.js
+++ projeto-03/Script.js
@@ -1,15 +1,17 @@
 const botao1 = document.getElementById('btn1')
 const botao2 = document.getElementById('btn2')
 const pag = document.getElementById('div1')
+const pagbtn = document.getElementById('div2')
 botao1.addEventListener('click', () => {
-    pag.classList.add('girar')
-    botao1.classList.add('girar')
-    botao1.classList.add('girar')
-
+    pag.classList.add('girar1')
+    pagbtn.classList.add('girar2')
+    pagbtn.classList.add('girar2')
+    document.body.classList.add('scroll')
 })
 
 botao2.addEventListener('click', () => {
-    pag.classList.remove('girar')
-    botao1.classList.remove('girar')
-    botao1.classList.remove('girar')
+    pag.classList.remove('girar1')
+    pagbtn.classList.remove('girar2')
+    pagbtn.classList.remove('girar2')
+    document.body.classList.remove('scroll')
 })
