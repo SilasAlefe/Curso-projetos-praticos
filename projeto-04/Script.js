49b8f488405696d752ecd760a27f05b25fd95eee
Silas Álefe <116173586+SilasAlefe@users.noreply.github.com>
2025-09-16 16:20:44 -0300

    16/09/2025-16:20

--- projeto-04/Script.js
+++ projeto-04/Script.js
@@ -1,14 +1,14 @@
 const btnsearch = document.getElementById('btn-search')
 const searchtxt = document.getElementById('searchtxt')
-searchtxt.classList.add('btn-search-activated')
+searchtxt.classList.add('btn-search-activated-origin')
 let clique = 1
 
 btnsearch.addEventListener('click', () => {
     if (clique==1){
-        searchtxt.classList.remove('btn-search-activated')
+        searchtxt.classList.add('btn-search-activated')
         clique += 1 
     } else if(clique){
-        searchtxt.classList.add('btn-search-activated')
+        searchtxt.classList.remove('btn-search-activated')
         clique = 1
     }
 })
